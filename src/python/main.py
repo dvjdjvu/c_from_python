cc80f6aec0d30788c6b326f46cd6129c093016c2
djvu <djvu@inbox.ru>
2019-09-13 16:38:03 +0300

    main.py

--- src/python/main.py
+++ src/python/main.py
@@ -10,57 +10,56 @@ sys.path.append('../../lib/')
 import time
 import _test
 
-if __name__ == '__main__':
-    # инициализация ffi
-    ffi = FFI()
-    
-    start_time = time.time()
-        
-    ##
-    # Работа с функциями
-    ##
-    
-    print('Работа с функциями:')
-    print('ret func_ret_int: ', _test.lib.func_ret_int(101))
-    print('ret func_ret_double: ', _test.lib.func_ret_double(12.123456789))
-    # Необходимо строку привести из cdata к массиву байтов, и массив байтов к строке.
-    print('ret func_ret_str: ', ffi.string(_test.lib.func_ret_str('Hello!'.encode('utf-8'))).decode("utf-8"))
-    print('ret func_many_args: ', _test.lib.func_many_args(15, 18.1617, 'X'.encode('utf-8'), 32000).decode("utf-8"))
-    
-    ##
-    # Работа с переменными
-    ##
 
-    print('\nРабота с переменными:')
-    print('ret a: ', _test.lib.a)
+# инициализация ffi
+ffi = FFI()
 
-    # Изменяем значение переменной.
-    _test.lib.a = 22
-    print('new a: ', _test.lib.a)
+start_time = time.time()
 
-    # Указываем, что переменная типа double
-    print('ret b: ', _test.lib.b)
+##
+# Работа с функциями
+##
 
-    # Указываем, что переменная типа char
-    print('ret c: ', _test.lib.c.decode("utf-8"))
-    
-    
-    ##
-    # Работа со структурами
-    ##
+print('Работа с функциями:')
+print('ret func_ret_int: ', _test.lib.func_ret_int(101))
+print('ret func_ret_double: ', _test.lib.func_ret_double(12.123456789))
+# Необходимо строку привести из cdata к массиву байтов, и массив байтов к строке.
+print('ret func_ret_str: ', ffi.string(_test.lib.func_ret_str('Hello!'.encode('utf-8'))).decode("utf-8"))
+print('ret func_many_args: ', _test.lib.func_many_args(15, 18.1617, 'X'.encode('utf-8'), 32000).decode("utf-8"))
 
-    print('\nРабота со структурами:')
-    
-    # Создаем структуру и заполняем её
-    test_st = _test.ffi.new("test_st_t *")
-    test_st.val1 = 5
-    test_st.val2 = 5.1234567
-    test_st.val3 = 'Z'.encode('utf-8')
-    
-    ret = _test.lib.func_ret_struct(test_st)
+##
+# Работа с переменными
+##
 
-    # Полученные данные из C
-    print('ret val1 = {}\nret val2 = {}\nret val3 = {}'.format(ret.val1, ret.val2, ret.val3.decode("utf-8")))
+print('\nРабота с переменными:')
+print('ret a: ', _test.lib.a)
 
-    print("--- %s seconds ---" % (time.time() - start_time))
-    
+# Изменяем значение переменной.
+_test.lib.a = 22
+print('new a: ', _test.lib.a)
+
+# Указываем, что переменная типа double
+print('ret b: ', _test.lib.b)
+
+# Указываем, что переменная типа char
+print('ret c: ', _test.lib.c.decode("utf-8"))
+
+
+##
+# Работа со структурами
+##
+
+print('\nРабота со структурами:')
+
+# Создаем структуру и заполняем её
+test_st = _test.ffi.new("test_st_t *")
+test_st.val1 = 5
+test_st.val2 = 5.1234567
+test_st.val3 = 'Z'.encode('utf-8')
+
+ret = _test.lib.func_ret_struct(test_st)
+
+# Полученные данные из C
+print('ret val1 = {}\nret val2 = {}\nret val3 = {}'.format(ret.val1, ret.val2, ret.val3.decode("utf-8")))
+
+print("--- %s seconds ---" % (time.time() - start_time))
