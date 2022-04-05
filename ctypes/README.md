b8d1024d135e9aa755bcae2f1cdfa95af58029c5
dvjdjvu <djvu@inbox.ru>
2022-04-05 03:55:57 -0700

    Readme update

--- ctypes/README.md
+++ ctypes/README.md
@@ -1,24 +1,31 @@
-Примеры вызова C/C++ из Python через ctypes.
+## Примеры вызова C/C++ из Python через ctypes.
 
-x86_x64 build
+## x86_x64
 ```bash
 make CC=gcc PP=g++
 ./src/python/x86_64/main.py
 ```
 
-android build
+## Android
+
+### GCC
+ - **armv7-a**
 ```bash
-make CC=arm-linux-gnueabi-gcc PP=arm-linux-gnueabi-g++
+make CC=arm-linux-gnueabi-gcc PP=arm-linux-gnueabi-g++ -D __ANDROID__=1
 ```
-or
+ - **armv8-a**
 ```bash
-make CC=aarch64-linux-gnu-gcc PP=aarch64-linux-gnu-g++
-```
-or use clang/clang++ android, but dont forget chanche path to clang in Makefile
-```bash
-make
+make CC=aarch64-linux-gnu-gcc PP=aarch64-linux-gnu-g++ -D __ANDROID__=1
 ```
 
+### Clang
+ - **aarch64-linux-android21**, dont forget chanche path to clang in Makefile
+```bash
+make -D __ANDROID__=1
+```
+
+
+Copy *.so in buildozer dir.
 ```bash
 make android
 ```
--- ctypes/src/c/test.c
+++ ctypes/src/c/test.c
@@ -9,33 +9,67 @@ double b = 5.12345;
 char c = 'X';
 
 int 
-func_ret_int(int val) { 
-    printf("C get func_ret_int: %d\n", val);
+func_ret_int(int val) {
+    #ifdef __ANDROID__
+        // Android Headers
+    #elif __APPLE__
+        // iOS Headers
+    #else
+        printf("C get func_ret_int: %d\n", val);
+    #endif
+
     return val;
 } 
 
 double 
-func_ret_double(double val) { 
-    printf("C get func_ret_double: %f\n", val);
+func_ret_double(double val) {
+    #ifdef __ANDROID__
+        // Android Headers
+    #elif __APPLE__
+        // iOS Headers
+    #else
+        printf("C get func_ret_double: %f\n", val);
+    #endif
+
     return val;
 } 
 
 char *
-func_ret_str(char *val) { 
-    printf("C get func_ret_str: %s\n", val);
+func_ret_str(char *val) {
+    #ifdef __ANDROID__
+        // Android Headers
+    #elif __APPLE__
+        // iOS Headers
+    #else
+        printf("C get func_ret_str: %s\n", val);
+    #endif
+
     return val;
 } 
 
 char
-func_many_args(int val1, double val2, char val3, short val4) { 
-    printf("C get func_many_args: int - %d, double - %f, char - %c, short - %d\n", val1, val2, val3, val4);
+func_many_args(int val1, double val2, char val3, short val4) {
+    #ifdef __ANDROID__
+        // Android Headers
+    #elif __APPLE__
+        // iOS Headers
+    #else
+        printf("C get func_many_args: int - %d, double - %f, char - %c, short - %d\n", val1, val2, val3, val4);
+    #endif
+
     return val3;
 } 
 
 test_st_t *
 func_ret_struct(test_st_t *test_st) {     
     if (test_st) {
-        printf("C get test_st: val1 - %d, val2 - %f, val3 - %c\n", test_st->val1, test_st->val2, test_st->val3);
+        #ifdef __ANDROID__
+            // Android Headers
+        #elif __APPLE__
+            // iOS Headers
+        #else
+            printf("C get test_st: val1 - %d, val2 - %f, val3 - %c\n", test_st->val1, test_st->val2, test_st->val3);
+        #endif
     }
     
     return test_st;
