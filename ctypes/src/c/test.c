9cfb6741ee4c3865fc0d4cbc89305dfe7587c2cd
dvjdjvu <djvu@inbox.ru>
2023-03-22 08:37:28 -0700

    Check callback work in Android 2

--- ctypes/src/c/test.c
+++ ctypes/src/c/test.c
@@ -11,10 +11,7 @@ char c = 'X';
 int 
 func_ret_int(int val) {
     #ifdef __ANDROID__
-        // Android Headers
-    #elif __APPLE__
-        // iOS Headers
-        printf("C get func_ret_int: %d\n", val);
+
     #else
         printf("C get func_ret_int: %d\n", val);
     #endif
@@ -25,10 +22,7 @@ func_ret_int(int val) {
 double 
 func_ret_double(double val) {
     #ifdef __ANDROID__
-        // Android Headers
-    #elif __APPLE__
-        // iOS Headers
-        printf("C get func_ret_double: %f\n", val);
+
     #else
         printf("C get func_ret_double: %f\n", val);
     #endif
@@ -39,10 +33,7 @@ func_ret_double(double val) {
 char *
 func_ret_str(char *val) {
     #ifdef __ANDROID__
-        // Android Headers
-    #elif __APPLE__
-        // iOS Headers
-        printf("C get func_ret_str: %s\n", val);
+
     #else
         printf("C get func_ret_str: %s\n", val);
     #endif
@@ -53,10 +44,7 @@ func_ret_str(char *val) {
 char
 func_many_args(int val1, double val2, char val3, short val4) {
     #ifdef __ANDROID__
-        // Android Headers
-    #elif __APPLE__
-        // iOS Headers
-        printf("C get func_many_args: int - %d, double - %f, char - %c, short - %d\n", val1, val2, val3, val4);
+
     #else
         printf("C get func_many_args: int - %d, double - %f, char - %c, short - %d\n", val1, val2, val3, val4);
     #endif
@@ -68,10 +56,7 @@ test_st_t *
 func_ret_struct(test_st_t *test_st) {     
     if (test_st) {
         #ifdef __ANDROID__
-            // Android Headers
-        #elif __APPLE__
-            // iOS Headers
-            printf("C get test_st: val1 - %d, val2 - %f, val3 - %c\n", test_st->val1, test_st->val2, test_st->val3);
+
         #else
             printf("C get test_st: val1 - %d, val2 - %f, val3 - %c\n", test_st->val1, test_st->val2, test_st->val3);
         #endif
@@ -86,10 +71,7 @@ func_callback(int (*f)(int, int)) {
     int ret = f(a, b);
 
     #ifdef __ANDROID__
-        // Android Headers
-    #elif __APPLE__
-        // iOS Headers
-        printf("C get func_callback: %d\n", ret);
+
     #else
         printf("C get func_callback: %d\n", ret);
     #endif
